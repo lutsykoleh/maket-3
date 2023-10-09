3555c3fc40ac02876faa8e08e62c7795753f8bf9
lutsykoleh <lutsykolehua@gmail.com>
2023-10-09 11:37:54 +0300

    slider add

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@ document.addEventListener("DOMContentLoaded", function(){
         document.querySelector("header").classList.toggle("open")
     })
 })
+
+$(document).ready(function(){
+    $('.slider__items').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        Infinity: true,
+        fade:true,
+        slidesToScroll: 1,
+        slidesToShow: 1,
+    })
+});
+
